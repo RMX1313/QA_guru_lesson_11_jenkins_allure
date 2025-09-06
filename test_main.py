25476f8fe770af97835493aeddc1d9f8970d4034
RMX <remix-92@mail.ru>
2025-09-06 03:00:13 +0300

    1

--- test_main.py
+++ test_main.py
@@ -1,77 +1,55 @@
+import allure
 from selene import browser, be, have
 import os
-from utils import attach
-image_path = os.path.abspath("textfile.txt")
-import allure
 from selene.support.shared import browser
+from selenium.webdriver.common.by import By
 
-def test_fill_form(setup_browser):
-    browser = setup_browser
-    browser.open('https://demoqa.com/automation-practice-form')
-    #Заполнение графы Имя
-    attach.add_screenshot(browser)
-    with allure.step("Заполняем имя"):
-        browser.element('[id="firstName"]').should(be.visible).type("Иван")
-    #Заполнение графы Фамилия
-    with allure.step("Заполняем фамилию"):
-        browser.element('[id="lastName"]').should(be.visible).type("Иванов")
-    #Заполнение графы почты
-    with allure.step("Заполняем почту"):
-        browser.element('[id="userEmail"]').should(be.visible).type("ivanov22@gmail.com")
-    #Выбор радиокнопки
-    with allure.step("Выбираем пол"):
-        browser.element('label[for="gender-radio-1"]').click()
-    #Заполнение графы телефон
-    with allure.step("Заполняем телефон"):
-        browser.element('[id="userNumber"]').should(be.visible).type("89098887678")
-    #Выбор даты рождения
-    with allure.step("Выбор даты рождения"):
-        browser.element('[id="dateOfBirthInput"]').click()
-        browser.element('.react-datepicker__day--021').should(be.visible).click()
-    #Выбор Subjects
-    with allure.step("Выбор предмета"):
-        browser.element('#subjectsInput').should(be.visible).type('Maths').press_enter()
-    #Выбор хобби
-    with allure.step("Выбор хобби"):
-        browser.element('label[for ="hobbies-checkbox-1"]').click()
-    #Загрузка файла
-    with allure.step("Загрузка файла"):
-        browser.element('[id = "uploadPicture"]').should(be.visible).send_keys(image_path)
-    #Заполнение адреса
-    with allure.step("Заполнение адреса"):
-        browser.element('[id="currentAddress"]').should(be.visible).type("ул.Пушкина д.Колотушкина кв.3")
-    #Выбор штата
-    with allure.step("Выбор штата"):
-        browser.element('#state input').type('NCR').press_enter()
-        #browser.element('[id="state"]').should(be.visible).click()
-        #browser.element('#react-select-3-input').type('NCR').press_enter()
-    #Выбор города
-    with allure.step("Выбор города"):
-        browser.element('[id="city"]').should(be.visible).click()
-        browser.element('[id="react-select-4-input"]').type('Delhi').press_enter()
-    #Отправка формы
-    with allure.step("Отправка формы"):
-        browser.element('#submit').click()
-    #browser.element('[id="submit"]').should(be.visible).click()
-    #Проверка отправки формы
-    with allure.step("Проверка отправки формы"):
-        browser.element('[id="example-modal-sizes-title-lg"]').should(be.visible).should(have.text('Thanks for submitting the form'))
 
-def test_successfully_filling(setup_browser):
-    with allure.step("Проверка формы"):
-        test_fill_form(setup_browser)
-        attach.add_screenshot(browser)
-        table_element = browser.all('table.table-dark tbody tr')
-        table_element.element_by(have.text('Student Name')).all('td').second.should(have.text('Иван Иванов'))
-        table_element.element_by(have.text('Student Email')).all('td').second.should(have.text('ivanov22@gmail.com'))
-        table_element.element_by(have.text('Gender')).all('td').second.should(have.text('Male'))
-        table_element.element_by(have.text('Mobile')).all('td').second.should(have.text('8909888767'))
-        table_element.element_by(have.text('Date of Birth')).all('td').second.should(have.text('21 August,2025'))
-        table_element.element_by(have.text('Subjects')).all('td').second.should(have.text('Maths'))
-        table_element.element_by(have.text('Hobbies')).all('td').second.should(have.text('Sports'))
-        table_element.element_by(have.text('Picture')).all('td').second.should(have.text('textfile.txt'))
-        table_element.element_by(have.text('Address')).all('td').second.should(have.text('ул.Пушкина д.Колотушкина кв.3'))
-        table_element.element_by(have.text('State and City')).all('td').second.should(have.text('NCR Delhi'))
+def test_form():
+    with allure.step('Открываем формум регистрации'):
+        file_path = os.path.join(os.path.dirname(__file__), 'file.txt')
+        browser.open('https://demoqa.com/automation-practice-form')
+    with allure.step('Заполнение формы'):
+        browser.element('#firstName').should(be.visible).type('Александр')
+        browser.element('#lastName').should(be.visible).type("Евдошенко")
+        browser.element('#userEmail').should(be.blank).type('remix-92@mail.ru')
+        browser.all('[name=gender]').element_by(have.value('Male')).element('..').click()
+        browser.element('#userNumber').should(be.blank).type('8800200100')
+        browser.element('#dateOfBirthInput').click()
+        browser.element('.react-datepicker__month-select').type('May')
+        browser.element('.react-datepicker__year-select').type('1992')
+        browser.element('.react-datepicker__day--013').click()
+        browser.element('#subjectsInput').type('Computer Science').press_enter()
+        browser.all('.custom-checkbox').element_by(have.exact_text('Sports')).click()
+        browser.element('#uploadPicture').send_keys(file_path)
+        browser.element('#currentAddress').type('ул.Жилая, 1')
 
+        # Получаем WebDriver элемент из Selene
+        driver = browser.driver
+        element = driver.find_element(By.CSS_SELECTOR, '#state')
+
+        # Прокручиваем элемент в видимую область
+        browser.execute_script("arguments[0].scrollIntoView(true);", element)
+
+
+        browser.element('#state').click()
+        browser.element('#react-select-3-option-0').click()
+        browser.element('#city').click()
+        browser.element('#react-select-4-option-0').click()
+        browser.element('#submit').press_enter()
+        browser.element('.modal-header').should(have.text('Thanks for submitting the form'))
+    with allure.step('Проверка реезультата'):
+        browser.all('.table-responsive td:nth-child(2)').should(have.texts(
+        'Александр Евдошенко',
+        'remix-92@mail.ru',
+        'Male',
+        '8800200100',
+        '13 May,1992',
+        'Computer Science',
+        'Sports',
+        'file.txt',
+        'ул.Жилая, 1',
+        'NCR Delhi'
+    ))
 
 
